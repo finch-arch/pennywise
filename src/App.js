8c7e8d6593d1588b5c0a2caddf8374f46c55488b
finch <vishalmahto003@gmail.com>
2023-12-12 13:26:55 +0530

    finished with context API

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { Balance } from './components/Balance';
 import { Header } from './components/Header';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
 function App() {
   return (
-    <div>
+    <GlobalProvider>
      <Header/>
+     <div className="container">
      <Balance/>
      <IncomeExpenses/>
      <TransactionList/>
-    </div>
+     <AddTransaction/>
+     </div>
+    </GlobalProvider>
   );
 }
 
